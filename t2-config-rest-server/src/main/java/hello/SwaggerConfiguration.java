5ca130f255c47ee5000b19daa3b29ca814841420
Eelco Brolman <e.brolman@telecats.nl>
2014-08-27 12:52:57 +0200

    Split Swagger and SwaggerUI config into separate files, this prevents AOP messing up the controller (somehow)

--- t2-config-rest-server/src/main/java/hello/SwaggerConfiguration.java
+++ t2-config-rest-server/src/main/java/hello/SwaggerConfiguration.java
@@ -1,18 +1,9 @@
 package hello;
 
-import static org.ajar.swaggermvcui.SwaggerSpringMvcUi.WEB_JAR_RESOURCE_LOCATION;
-import static org.ajar.swaggermvcui.SwaggerSpringMvcUi.WEB_JAR_RESOURCE_PATTERNS;
-import static org.ajar.swaggermvcui.SwaggerSpringMvcUi.WEB_JAR_VIEW_RESOLVER_PREFIX;
-import static org.ajar.swaggermvcui.SwaggerSpringMvcUi.WEB_JAR_VIEW_RESOLVER_SUFFIX;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
 import com.mangofactory.swagger.plugin.EnableSwagger;
@@ -22,7 +13,7 @@ import com.wordnik.swagger.model.ApiInfo;
 @Configuration
 @EnableWebMvc
 @EnableSwagger
-public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
+public class SwaggerConfiguration {
 
     private SpringSwaggerConfig springSwaggerConfig;
 
@@ -43,22 +34,4 @@ public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
         return apiInfo;
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(WEB_JAR_RESOURCE_PATTERNS).addResourceLocations(WEB_JAR_RESOURCE_LOCATION)
-                .setCachePeriod(0);
-    }
-
-    @Bean
-    public InternalResourceViewResolver getInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix(WEB_JAR_VIEW_RESOLVER_PREFIX);
-        resolver.setSuffix(WEB_JAR_VIEW_RESOLVER_SUFFIX);
-        return resolver;
-    }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
 }

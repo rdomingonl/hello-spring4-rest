dccb030dc9d91da1e78af45bb88d2238c30855c4
Raymond Domingo <raymond@telecats.nl>
2014-08-27 16:04:31 +0200

    Detect changes to customer

--- t2-config-rest-server/src/main/java/hello/Application.java
+++ t2-config-rest-server/src/main/java/hello/Application.java
@@ -1,14 +1,13 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import hello.jpa.CustomerChangeListener;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -21,18 +20,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Import(RepositoryRestMvcConfiguration.class)
 public class Application {
 
-    @Autowired
-    Greeting g;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
     }
 
     @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public Greeting getG() {
-        return new Greeting(1, "");
+    CustomerChangeListener customerChangeListener() {
+        return new CustomerChangeListener();
     }
 
     // @Bean
---
+++ t2-config-rest-server/src/main/java/hello/jpa/CustomerChangeListener.java
@@ -0,0 +1,62 @@
+package hello.jpa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
+
+@RepositoryEventHandler
+public class CustomerChangeListener extends AbstractRepositoryEventListener<Customer> {
+
+    Logger logger = LoggerFactory.getLogger(CustomerChangeListener.class);
+
+    @Override
+    protected void onBeforeCreate(Customer entity) {
+        logger.info("onBeforeCreate");
+    }
+
+    @Override
+    protected void onAfterCreate(Customer entity) {
+        logger.info("onAfterCreate");
+    }
+
+    @Override
+    public void onBeforeSave(Customer entity) {
+        logger.info("onBeforeSave");
+    }
+
+    @Override
+    public void onAfterSave(Customer entity) {
+        logger.info("onAfterSave");
+    }
+
+    @Override
+    protected void onBeforeLinkSave(Customer parent, Object linked) {
+        logger.info("onBeforeLinkSave");
+    }
+
+    @Override
+    protected void onAfterLinkSave(Customer parent, Object linked) {
+        logger.info("onAfterLinkSave");
+    }
+
+    @Override
+    protected void onBeforeLinkDelete(Customer parent, Object linked) {
+        logger.info("onBeforeLinkDelete");
+    }
+
+    @Override
+    protected void onAfterLinkDelete(Customer parent, Object linked) {
+        logger.info("onAfterLinkDelete");
+    }
+
+    @Override
+    protected void onBeforeDelete(Customer entity) {
+        logger.info("onBeforeDelete");
+    }
+
+    @Override
+    protected void onAfterDelete(Customer entity) {
+        logger.info("onAfterDelete");
+    }
+}
